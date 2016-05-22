446509c99ee33b25aed08f18a10a326f1dbfd032
awesomels <liushan201319@gmail.com>
2016-05-22 23:09:15 +0800

    doing connect...

--- proxy_main.c
+++ proxy_main.c
@@ -3,9 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <fcntl.h>
+#include <errno.h>
 
 
 int main(){
@@ -30,6 +33,7 @@ int main(){
 	user_addr.sin_family = AF_INET;
 	user_addr.sin_port = htons(Port);
 	user_addr.sin_addr.s_addr = htol(INADDR_ANY);
+	
 	/* 套接字绑定端口 */
 	ret = bind(usersockfd, (struct sockaddr*)&user_addr, sizeof(user_addr));
 	if(ret != 0){
@@ -47,13 +51,126 @@ int main(){
 		exit(1);
 	}
 
+	/* 设置套接字为非阻塞 */
+	setNonBlocking(usersockfd);
+
 	/*
-	 *
+	 *使用epoll
 	 */
+	/* 声明epoll_wait结构体的变量，ev用于注册事件，数组指针用于回传要处理的事件 */
+	struct epoll_event ev, *events;
+	int epfd = epoll_create1(0);//除了参数size被忽略外，此函数和epoll_create完全相同
+	if(epfd == -1){
+		perror("epoll_create");
+		abort();
+	}
+	ev.data.fd = usersockfd;    //设置与要处理的事件相关的文件描述符
+	ev.events = EPOLLIN|EPOLLET;//设置要处理的事件类型
+	/* 注册epoll事件 */
+	ret = epoll_ctl(epfd, EPOLL_CTL_ADD, usersockfd, &ev);
+	if(ret == -1){
+		perror("epoll_ctl");
+		abort();
+	}
+	/* Buffer where events are returned */
+	events = calloc(MAXENENTS, sizeof(event));
+	/* the event loop */
+	int nfds, i, connfd, n;
+	for ( ; ; ) {
+		nfds = epoll_wait(epfd, events, 20, 500);
+		for(i = 0; i < nfds; ++i) {
+			if(events[i].data.fd == usersockfd){   //有新的连接
+				connfd = accept(usersockfd, (sockaddr*)&cli_addr, &cli_len); //accept这个连接
+				setNonBlocking(connfd);   //设置为非阻塞
+				ev.data.fd = connfd;
+				ev.events = EPOLL|EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &ev);   //将新的fd添加到epoll的监听队列
+			} else if (events[i].events & EPOLLIN) {   //已经连接的用户，接收到数据，读socket
+				
+			} else if (events[i].events & EPOLLOUT) {  //有数据发送
+				
+			}
+		}//nfds for
+	}//for
 
 	return 0;
 }
 
-static void* UtoR(){
+/* usersockfd向remtsockfd传送数据，http请求 */
+static void* UtoR(void* arg){
+	struct mUtoR *pstru;
+	pstru = (struct mUtoR *)arg;
+	char buf[BUF_SIZE];
+	char ipstr[32];
+	int flag = 1;
+	while (recv(pstru->mUsocket, buf, sizeof(buf), 0) > 0) {
+		if(flag){
+			DNtoIP(buf, ipstr);            //域名转IP
+			struct sockaddr_in toRemt_addr;//利用remtsockfd发送的地址
+			bzero(&toRemt_addr, sizeof(toRemt_addr));
+			toRemt_addr.sin_family = AF_INET;
+			toRemt_addr.sin_port = htons(80);
+			toRemt_addr.sin_addr.s_addr = htonl(inet_addr(ipstr));
+			flag = 0;
+			/* 连接服务器 connect */
+			conn
+		}
+	}//while(recv)
 	
+	
+}
+
+/* remtsockfd向usersockfd传送数据，http响应 */
+static void* RtoU(void *arg){
+
+}
+
+/* 设置socket为非阻塞 */
+void setNonBlocking(int mSocket){
+	int flags, s;
+	//获取文件标志位
+	flags = fcntl(mSocket, F_GETFL, 0);
+	if(flags == -1){
+		perror("fcntl");
+		return -1;
+	}
+	//设置文件状态标志
+	flags |= O_NONBLOCK;
+	s = fcntl(sfd, F_SETFL, flags);
+	if(s == -1){
+		perror("fcntl");
+		return -1;
+	}
+	return 0;
+}
+
+/* 域名转Ip函数 */
+char *DNtoIP(char *mString, char *ipstr){
+	struct hostent *hptr;
+	//char *ipstr = (char*)calloc(32,sizeof(char*));   //记得在使用后free掉ipstr
+	char *substr = "Host: ";
+	char *src = strstr(mString, substr) + 6;
+	char *pchar = src;
+	int length = 0;
+	while(*pchar != '\r'){
+		length++;
+		pchar++;
+	}
+	char *domainName = (char*)malloc(length);
+	strncpy(domainName, src, length);
+	if((hptr = gethostbyname(domainName)) == NULL){
+		perror("gethostbyname");
+		return 0;
+	}
+	switch(hptr->h_addrtype){
+		case AF_INET:
+		case AF_INET6:
+				inet_ntop(hptr->h_addrtype, hptr->h_addr, ipstr,sizeof(ipstr));
+				break;
+		default:
+				perror("unknown address type\n");
+				break;
+	}
+	free(domainName);
+	return ipstr;
 }
--- proxy_main.h
+++ proxy_main.h
@@ -1,7 +1,49 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-#include <stdlib.h>
+#define MAXEVENTS 64
+#define BUF_SIZE 4096
 
+/* UtoR线程参数 */
+typedef struct mUtoR{
+	int mUsocket;
+	int mRsocket;
+}mUtoR_t;
 
+/* RtoU线程参数 */
+typedef struct mRtoU{
+	int mSocket;
+}mRtoU_t;
+/* UtoR线程参数 */
+typedef struct mUtoR{
+	int mSocket;
+}mUtoR_t;
+
+/*
+ *@brief:  域名转IP, 提供给发送socket
+ *@param:  mString, 供提取域名的字符串,未筛选
+ *@return: ipstr[32], 返回的ip地址,32位字符数组
+ */
+char *DNtoIP(mString);
+
+/*
+ *@brief:  设置socket为非阻塞模式
+ *@param:  mSocket套接字
+ *@return: 0:正常
+ */
+void setNonBlocking(int mSocket);
+
+/*
+ *@brief:  从usersockfd向remtsockfd传送http请求
+ *@param:  
+ *@return: void
+ */
+static void* UtoR (void* arg);
+
+/*
+ *@brief:  从remtsockfd向usersockfd传送http响应
+ *@param:
+ *@return: void
+ */
+static void* RtoU (void* arg);
 #endif
